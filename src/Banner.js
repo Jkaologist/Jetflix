ae7e3e6db447e93fb080d40ddaedff825364987a
Jon K <Johnakao@gmail.com>
2021-10-18 12:07:37 -0700

    Added styling to the banner component

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState([]);
@@ -17,6 +18,10 @@ function Banner() {
     fetchData();
   }, []);
 
+  function truncate(str, num) {
+    return str?.length > num ? str.substr(0, num - 1) + "..." : str;
+  }
+
   return (
     <header
       className="banner"
@@ -26,13 +31,16 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner_contents"></div>
-      <h1> {movie?.title || movie?.name || movie?.original_name} </h1>
-      <div className="banner_buttons">
-        <button className="banner_button">Play</button>
-        <button className="banner_button">My List</button>
+      <div className="banner_contents">
+        <h1 className="banner_title">
+          {movie?.title || movie?.name || movie?.original_name}{" "}
+        </h1>
+        <div className="banner_buttons">
+          <button className="banner_button">Play</button>
+          <button className="banner_button">My List</button>
+        </div>
+        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
       </div>
-      <h1 className="banner_description">{movie?.overview}</h1>
     </header>
   );
 }
