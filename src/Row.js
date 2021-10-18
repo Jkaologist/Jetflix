5c62c10a6aafb96265d1fcb8b42205bc193b1626
Jon K <Johnakao@gmail.com>
2021-10-18 10:50:22 -0700

    Logging the axios request to console

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
+import axios from "./axios";
 
 function Row({ title, fetchURL }) {
   const [movies, setMovies] = useState([]);
 
   // A snippet of code which runs based on a specific condition/variable
   useEffect(() => {
-    //make a request to load the movies
+    //make an asynchronous request to load the movies
+    async function fetchData() {
+      // await the promise
+      const request = await axios.get(fetchURL);
+      console.log(request);
+    }
+    fetchData();
   }, []);
 
   return (
